84c3a8ce79072674e0fc6dd2f8cd0e2ff08a8fdf
Arnel  Basiliote <arnelbasiliote@Arnels-MacBook-Pro.local>
2016-02-22 23:59:39 +0800

    init Blog Controller

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BiboBlog\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use BiboBlog\Http\Requests;
+use BiboBlog\Http\Controllers\Controller;
+
+class BlogController extends Controller
+{
+    //
+}
